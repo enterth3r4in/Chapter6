dd7ff0e9d9009959cd26a7c68321d2cf09fae433
enterth3r4in <believe.the.rain@gmail.com>
2015-10-20 13:39:51 -0400

    Messing with arrays

--- Chapter6/Chapter6/Chapter6.py
+++ Chapter6/Chapter6/Chapter6.py
@@ -1,13 +1,18 @@
 #Fun With Arrays
 
 animal_array = ["Giraffe", "Emu", "Ostrich", "Chimpanzee", "Elephant"]
-
-print(animal_array)
-
-for i in animal_array:
-    print(i)
+#print(animal_array)
+#for i in animal_array:
+#    print(i)
 
 animal_array.append("Hyena")
 animal_array.append("Kangaroo")
-
 print(animal_array)
+
+animal_array.remove("Emu")
+#print(animal_array)
+#print("Number of elements: ", len(animal_array))
+#print("Last index: ", len(animal_array) - 1)
+
+for i in range(len(animal_array) + 1):
+    print(animal_array[i])
