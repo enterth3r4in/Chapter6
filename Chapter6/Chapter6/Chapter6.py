d9d79765c9980db9f705045c8fe2ae023f43dc26
enterth3r4in <believe.the.rain@gmail.com>
2015-10-20 13:26:52 -0400

    Array Fun
    Manipulating arrays in python obviously

---
+++ Chapter6/Chapter6/Chapter6.py
@@ -0,0 +1,15 @@
+#Fun With Arrays
+
+animal_array = ["Giraffe", "Emu", "Ostrich", "Chimpanzee", "Elephant"]
+
+print(animal_array)
+
+for i in animal_array:
+    print(i)
+
+animal_array.append("Hyena")
+animal_array.append("Kangaroo")
+
+print()
+for i in animal_array:
+    print(i)
